0f0b041963b1db5ae3723c6a344380443176b050
loganjf <loganfickling@gmail.com>
2018-10-25 10:39:30 -0400

    Modify to include some required deps

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ REQUIRES_PYTHON = '>=3.6.0'
 VERSION = None
 
 # What packages are required for this module to be executed?
-REQUIRED = [
+REQUIRED = ['pyedflib', 'mne', 'ptsa', 'scipy', 'numpy', 'xarray','pandas'
     # 'requests', 'maya', 'records',
 ]
 
